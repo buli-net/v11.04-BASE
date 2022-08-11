5eb127593caf69f9147a5a1bad4a7042458e176d
Andreas Schildbach <andreas.schildbach@bloq.com>
2022-08-11 21:54:47 +0200

    org.bitcoinj.core.AddressFormatException → org.bitcoinj.base.exceptions.AddressFormatException

--- wallet/src/de/schildbach/wallet/ui/send/SweepWalletFragment.java
+++ wallet/src/de/schildbach/wallet/ui/send/SweepWalletFragment.java
@@ -58,7 +58,7 @@ import de.schildbach.wallet.ui.TransactionsAdapter;
 import de.schildbach.wallet.ui.scan.ScanActivity;
 import de.schildbach.wallet.util.MonetarySpannable;
 import de.schildbach.wallet.util.Toast;
-import org.bitcoinj.core.AddressFormatException;
+import org.bitcoinj.base.exceptions.AddressFormatException;
 import org.bitcoinj.core.DumpedPrivateKey;
 import org.bitcoinj.core.ECKey;
 import org.bitcoinj.core.NetworkParameters;
