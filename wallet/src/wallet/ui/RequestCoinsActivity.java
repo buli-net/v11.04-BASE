48e352880ec876572c32bab26773e2b697d718c9
Andreas Schildbach <andreas@schildbach.de>
2025-03-01 17:08:17 +0100

    AndroidManifest.xml: strip middle packages from Java package
    This change doesn't affect the application ID. It could however
    cause launchers to drop shortcuts and widgets, so users need to
    re-create them. Modern launchers seem to transparently handle the
    migration.

---
+++ wallet/src/wallet/ui/RequestCoinsActivity.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package wallet.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+import androidx.activity.EdgeToEdge;
+import androidx.activity.SystemBarStyle;
+import androidx.annotation.Nullable;
+import androidx.core.graphics.Insets;
+import androidx.core.view.MenuProvider;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
+import wallet.R;
+import org.bitcoinj.base.ScriptType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Andreas Schildbach
+ */
+public final class RequestCoinsActivity extends AbstractWalletActivity {
+    public static final String INTENT_EXTRA_OUTPUT_SCRIPT_TYPE = "output_script_type";
+
+    public static void start(final Context context) {
+        start(context, null);
+    }
+
+    public static void start(final Context context, final @Nullable ScriptType outputScriptType) {
+        final Intent intent = new Intent(context, RequestCoinsActivity.class);
+        if (outputScriptType != null)
+            intent.putExtra(INTENT_EXTRA_OUTPUT_SCRIPT_TYPE, outputScriptType);
+        context.startActivity(intent);
+    }
+
+    private RequestCoinsActivityViewModel viewModel;
+
+    private static final Logger log = LoggerFactory.getLogger(RequestCoinsActivity.class);
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        EdgeToEdge.enable(this, SystemBarStyle.dark(getColor(R.color.bg_action_bar)),
+                SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT));
+        super.onCreate(savedInstanceState);
+        log.info("Referrer: {}", getReferrer());
+        setContentView(R.layout.request_coins_content);
+        final Toolbar appbar = findViewById(R.id.request_coins_appbar);
+        appbar.getNavigationIcon().setTint(getColor(R.color.fg_on_dark_bg_network_significant));
+        setActionBar(appbar);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (v, windowInsets) -> {
+            final Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            final boolean imeVisible = windowInsets.isVisible(WindowInsetsCompat.Type.ime());
+            v.setPadding(v.getPaddingLeft(), insets.top, v.getPaddingRight(),
+                    imeVisible ? windowInsets.getInsets(WindowInsetsCompat.Type.ime()).bottom : 0);
+            return windowInsets;
+        });
+
+        viewModel = new ViewModelProvider(this).get(RequestCoinsActivityViewModel.class);
+        viewModel.showHelpDialog.observe(this, new Event.Observer<Integer>() {
+            @Override
+            protected void onEvent(final Integer messageResId) {
+                HelpDialogFragment.page(getSupportFragmentManager(), messageResId);
+            }
+        });
+
+        addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(final Menu menu, final MenuInflater inflater) {
+                inflater.inflate(R.menu.request_coins_activity_options, menu);
+            }
+
+            @Override
+            public void onPrepareMenu(final Menu menu) {
+            }
+
+            @Override
+            public boolean onMenuItemSelected(final MenuItem item) {
+                if (item.getItemId() == R.id.request_coins_options_help) {
+                    viewModel.showHelpDialog.setValue(new Event<>(R.string.help_request_coins));
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
---
+++ wallet/src/wallet/ui/send/SendCoinsActivity.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package wallet.ui.send;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+import androidx.activity.EdgeToEdge;
+import androidx.activity.SystemBarStyle;
+import androidx.annotation.Nullable;
+import androidx.core.graphics.Insets;
+import androidx.core.view.MenuProvider;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
+import wallet.Constants;
+import wallet.R;
+import wallet.data.PaymentIntent;
+import wallet.service.BlockchainService;
+import wallet.ui.AbstractWalletActivity;
+import wallet.ui.Event;
+import wallet.ui.HelpDialogFragment;
+import org.bitcoinj.base.Coin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Andreas Schildbach
+ */
+public final class SendCoinsActivity extends AbstractWalletActivity {
+    public static final String INTENT_EXTRA_PAYMENT_INTENT = "payment_intent";
+    public static final String INTENT_EXTRA_FEE_CATEGORY = "fee_category";
+
+    private static final Logger log = LoggerFactory.getLogger(SendCoinsActivity.class);
+
+    public static void start(final Context context, final PaymentIntent paymentIntent,
+            final @Nullable FeeCategory feeCategory, final int intentFlags) {
+        final Intent intent = startIntent(context, paymentIntent, feeCategory, intentFlags);
+        context.startActivity(intent);
+    }
+
+    public static Intent startIntent(final Context context, final PaymentIntent paymentIntent,
+                                      final FeeCategory feeCategory, final int intentFlags) {
+        final Intent intent = new Intent(context, SendCoinsActivity.class);
+        intent.putExtra(INTENT_EXTRA_PAYMENT_INTENT, paymentIntent);
+        if (feeCategory != null)
+            intent.putExtra(INTENT_EXTRA_FEE_CATEGORY, feeCategory);
+        if (intentFlags != 0)
+            intent.setFlags(intentFlags);
+        return intent;
+    }
+
+    public static void start(final Context context, final PaymentIntent paymentIntent) {
+        start(context, paymentIntent, null, 0);
+    }
+
+    public static void startDonate(final Context context, final Coin amount, final @Nullable FeeCategory feeCategory,
+            final int intentFlags) {
+        start(context, PaymentIntent.from(Constants.DONATION_ADDRESS,
+                context.getString(R.string.wallet_donate_address_label), amount), feeCategory, intentFlags);
+    }
+
+    private SendCoinsActivityViewModel viewModel;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        EdgeToEdge.enable(this, SystemBarStyle.dark(getColor(R.color.bg_action_bar)),
+                SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT));
+        super.onCreate(savedInstanceState);
+        log.info("Referrer: {}", getReferrer());
+        setContentView(R.layout.send_coins_content);
+        final Toolbar appbar = findViewById(R.id.send_coins_appbar);
+        appbar.getNavigationIcon().setTint(getColor(R.color.fg_on_dark_bg_network_significant));
+        setActionBar(appbar);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (v, windowInsets) -> {
+            final Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(v.getPaddingLeft(), insets.top, v.getPaddingRight(), v.getPaddingBottom());
+            return windowInsets;
+        });
+
+        viewModel = new ViewModelProvider(this).get(SendCoinsActivityViewModel.class);
+        viewModel.showHelpDialog.observe(this, new Event.Observer<Integer>() {
+            @Override
+            protected void onEvent(final Integer messageResId) {
+                HelpDialogFragment.page(getSupportFragmentManager(), messageResId);
+            }
+        });
+
+        addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(final Menu menu, final MenuInflater inflater) {
+                inflater.inflate(R.menu.send_coins_activity_options, menu);
+            }
+
+            @Override
+            public void onPrepareMenu(final Menu menu) {
+            }
+
+            @Override
+            public boolean onMenuItemSelected(final MenuItem item) {
+                if (item.getItemId() == R.id.send_coins_options_help) {
+                    viewModel.showHelpDialog.setValue(new Event<>(R.string.help_send_coins));
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        BlockchainService.start(this, false);
+    }
+}
